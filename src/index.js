e6cfd0ede384276840c40c85d33c4f4936c988f7
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-26 22:32:16 +0200

    commit-one

--- src/index.js
+++ src/index.js
@@ -4,30 +4,29 @@ import './styles.css';
 
 const rootElement = document.querySelector('#root');
 
-const renderSeconds = time => {
-  const seconds = new Date(time).getSeconds();
-  const backgroundColor = seconds % 2 === 0 
-        ? '#fff'
-        : "#000";
-  const textColor = seconds % 2 !== 0 
-        ? '#fff'
-        : "#000";
-  const styles = {
-    color: textColor,
-    backgroundColor
-  };
+// const renderSeconds = time => {
+  // const seconds = new Date(time).getSeconds();
+  // const backgroundColor = seconds % 2 === 0 
+  //       ? '#fff'
+  //       : "#000";
+  // const textColor = seconds % 2 !== 0 
+  //       ? '#fff'
+  //       : "#000";
+  // const styles = {
+  //   color: textColor,
+  //   backgroundColor
+  // };
   const element = (
-    <div 
-    className="seconds"
-    style={styles}
-    >
-     {seconds}
+    <div>
+      <h1 class="title">Todo List</h1>
+      <main class="todo-list">
+       <div class="actions">
+           <input class="task-input" type="text"/>
+           <button class="btn create-task-btn">Create</button>
+       </div>
+      <ul class="list"></ul>
+      </main>
     </div>
   );
   
   ReactDOM.render(element, rootElement);
-}
-
-renderSeconds(new Date());
-
-setInterval(() => renderSeconds(new Date()), 1000);
